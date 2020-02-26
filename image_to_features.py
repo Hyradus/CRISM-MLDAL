1924101053435588b144bbde3e07de066e0c2ab3
Hyradus <58908028+Hyradus@users.noreply.github.com>
2020-02-26 17:38:23 +0100

    Update image_to_features.py
    - def read_single_image, work both with *.npy and *.png
    - def read_images works only with *.png

--- image_to_features.py
+++ image_to_features.py
@@ -1,27 +1,52 @@
 import pandas
 import numpy as np
-from skimage.io import imread, imsave, imshow
+import matplotlib.pyplot as plt
+import cv2 as cv
 path= r'D:\TEMP\CRISM_Selected_Datasets\2008_067\processed'
 spectral_index = ('OLINDEX3', 'LCPINDEX2', 'HCPINDEX2', 'BD2100_2', 'BD1900_2', 'BDI1000VIS', 'D2300', 'SINDEX2', 'R770')
+
 # read the 9 index-images (either original or the thresholded)img
-def read_images():
-    
-    #if file_type == 'dump':
-    #    print('dump')
-    #elif file_type == '.npy':
-    #img=np.load(path + '/' + '/R770' +'_' + image_type + '.' + file_type)
-    img=np.load(path + '/R770_thresholded.npy', allow_pickle=False)
-    imshow(img)
-       
+def read_single_image(spectral_index, data_type, file_type):
+
+    if file_type == 'dump':
+        print('dump')
+    elif file_type == 'npy':
+        img = np.load(path + '/' + spectral_index + '_' + data_type + '.' + file_type, allow_pickle=False)
+    elif file_type == 'png':
+        img = cv.imread(path + '/' + spectral_index + '_' + data_type + '.' + file_type)
+    plt.imshow(img)
     return (img)
+
+# read all images into one npy array. works with png, not work with npy
+def read_images(data_type, file_type):
+    imgs =[]
+    for i in spectral_index:
+        if file_type == 'dump':
+            print('dump file')
+        elif file_type == 'npy':
+            img = np.load(path + '/' + spectral_index + '_' + data_type + '.' + file_type, allow_pickle=False)
+            imgs.concatenate[img]
+        elif file_type == 'png':
+            img = cv.imread(path + '/' + i + '_' + data_type + '.' + file_type, cv.IMREAD_GRAYSCALE)
+            imgs.append(img)
+
+    return (imgs)
+
+
+
+
+
+
+
+
 
 
 def array_to_series(lbl,arr):
     vec = arr.flatten()
     ser = pandas.Series(data=vec,name=lbl)
-    
+
 def merge_series_to_df(df,ser):
-    assert df is not None 
+    assert df is not None
     df[ser.name] = ser
     return df
 
@@ -32,7 +57,7 @@ def X(directory=""):
         ser = array_to_series(image_array)
         assert len(ser) == len(df)
         df = merge_series_to_df(df,ser)
-     
+
 def define_class_array(index_array):
     return index_array.astype(bool)
 
@@ -42,7 +67,7 @@ def Y(mineral_name, index_filename=""):
     arr = define_class_array(image_array_index)
     ser = array_to_series(mineral_name, arr)
     return ser
-    
+
 dfX = X("images_directory/")
 dfX.to_csv("index_features-csv")
 
@@ -50,3 +75,7 @@ for mineral in list_minerals:
     dfY = Y()
     assert len(dfY) == len(dfX)
     dfY.to_csv(mineral.csv)
+
+    imm = cv.imread(path + '/HCPINDEX2_thresholded.png')
+    cv.imshow('image', imm)
+    cv.waitKey(0)
